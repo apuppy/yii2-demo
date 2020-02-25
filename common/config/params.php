93ff92a8ff5087d8c00e186d45c0178c9067a5dc
lihongde <lihongde@leweikeji.com>
2020-02-25 11:30:21 +0800

    RabbitmqTarget bugfix

--- common/config/params.php
+++ common/config/params.php
@@ -5,13 +5,14 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     // log target(\common\logging\RabbitmqTarget)用到的配置参数
     'amqp' => [
-        'host' => '192.168.33.22',
+        'host' => '127.0.0.1',
         'port' => 5672,
         'user' => 'demo',
         'password' => 'demo123',
         'vhost' => '/',
         'exchange' => 'yii-log-exchange',
-        'queue' => 'yii-log-exchange',
+        'queue' => 'yii-log-queue',
+        'log_from' => 'php-amqp', // 标记字段
         'log_extra' => [ // 附加日志字段,可配多个键值对
             //'key_name' => 'key_value',
         ]
