80a98253f5d83eb8e24244ae590793b4ce0f1786
lihongde <lihongde@leweikeji.com>
2020-06-08 19:01:48 +0800

    add variable check use instanceof

--- common/services/ErrorLogService.php
+++ common/services/ErrorLogService.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace common\services;
-
 
 use common\models\ErrorLog;
 use Exception;
+use yii\base\ErrorException;
 
 class ErrorLogService
 {
@@ -35,18 +34,20 @@ class ErrorLogService
      */
     public static function record_queue_error($error)
     {
-        $error_info = [
-            'module' => 'queue',
-            'level' => 'error',
-            'code' => $error->getCode(),
-            'message' => $error->getMessage(),
-            'file' => $error->getFile(),
-            'line' => $error->getLine(),
-            'trace' => $error->getTraceAsString(),
-            'date' => date('Y-m-d'),
-            'created_at' => date('Y-m-d H:i:s')
-        ];
-        static::record($error_info);
+        if ($error instanceof ErrorException) {
+            $error_info = [
+                'module' => 'queue',
+                'level' => 'error',
+                'code' => $error->getCode(),
+                'message' => $error->getMessage(),
+                'file' => $error->getFile(),
+                'line' => $error->getLine(),
+                'trace' => $error->getTraceAsString(),
+                'created_date' => date('Y-m-d'),
+                'created_at' => date('Y-m-d H:i:s')
+            ];
+            static::record($error_info);
+        }
     }
 
 }
